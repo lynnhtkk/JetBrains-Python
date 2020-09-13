8762d75661afacfe37fdc1b2bba4924fb79c5c6f
lynnhtkk <nyi20471@gmail.com>
2020-09-13 09:51:31 +0630

    added transpose option to numeric_matrix_processor

--- One File Programs (Easy)/numeric_matrix_processor.py
+++ One File Programs (Easy)/numeric_matrix_processor.py
@@ -1,6 +1,7 @@
 # Stage 3/6 of Numeric Matrix Processor
 # Include choosing options for the type of operations
 # Include Matrix Multiplication by ANOTHER MATRIX
+# Added Transpose Option
 
 class Matrix:
     result_mat = []
@@ -10,10 +11,12 @@ class Matrix:
 
     def get_options(self):
         while True:
-            print('1. Add matrices')
-            print('2. Multiply matrix by a constant')
-            print('3. Multiply matrices')
-            print('0. Exit')
+            print("""
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+4. Transpose matrix
+0. Exit""")
             self.opt = input('Your choice: ')
             if self.opt == '0':
                 break
@@ -23,12 +26,14 @@ class Matrix:
                 self.mult_matrix_constant()
             elif self.opt == '3':
                 self.mult_matrix()
+            elif self.opt == '4':
+                self.transpose_matrix()
             
 
     def get_matrix(self, order):
-        self.row, self.col = input(f'Enter size of {order} matrix: ').split()
+        self.row, self.col = map(int, input(f'Enter size of {order} matrix: ').split())
         print(f'Enter {order} matrix:')
-        self.mat = [input().split() for _ in range(int(self.row))]
+        self.mat = [input().split() for _ in range(self.row)]
 
     def sum_matrix(self):
         Matrix.result_mat = []
@@ -38,23 +43,19 @@ class Matrix:
         if self.row != second.row or self.col != second.col:
             print('The operation cannot be performed.\n')
         else:
-            for i in range(int(self.row)):
+            for i in range(self.row):
                 Matrix.result_mat.append([])
-                for j in range(int(self.col)):
+                for j in range(self.col):
                     Matrix.result_mat[i].append(float(self.mat[i][j]) + float(second.mat[i][j]))
-            print('The result is: ')
-            for r in Matrix.result_mat:
-                print(' '.join(map(str, r)))
-            print('')
+            
+            self.format_matrix()
 
     def mult_matrix_constant(self):
         self.get_matrix('')
         num = float(input('Enter constant: '))
         Matrix.result_mat = [[num * float(self.mat[i][j]) for j in range(int(self.col))] for i in range(int(self.row))]
-        print('The result is: ')
-        for r in Matrix.result_mat:
-                print(' '.join(map(str, r)))
-        print('')
+        
+        self.format_matrix()
 
     def mult_matrix(self):
         Matrix.result_mat = []
@@ -64,17 +65,62 @@ class Matrix:
         if self.col != second.row:
             print('The operation cannot be performed.\n')
         else:
-            for i in range(int(self.row)):
+            for i in range(self.row):
                 Matrix.result_mat.append([])
-                for j in range(int(second.col)):
+                for j in range(second.col):
                     temp = 0
-                    for k in range(int(self.col)):
+                    for k in range(self.col):
                         temp += float(self.mat[i][k]) * float(second.mat[k][j])
                     Matrix.result_mat[i].append(temp)
+        
+        self.format_matrix()
+
+    def transpose_matrix(self):
+        print("""
+1. Main diagonal
+2. Side diagonal
+3. Vertical line
+4. Horizontal line""")
+        toptions = input('Your choice: ')
+        if toptions == '1':
+            Matrix.result_mat = []
+            self.get_matrix('')
+            for i in range(self.col):
+                Matrix.result_mat.append([])
+                for j in range(self.row):
+                    Matrix.result_mat[i].append(self.mat[j][i])
+
+            self.format_matrix()
+        elif toptions == '2':
+            Matrix.result_mat = []
+            self.get_matrix('')
+            tmp_row = 0
+            for i in range(self.col - 1, -1, -1):
+                Matrix.result_mat.append([])
+                for j in range(self.row - 1, -1, -1):
+                    Matrix.result_mat[tmp_row].append(self.mat[j][i])
+                tmp_row += 1
+
+            self.format_matrix()
+        elif toptions == '3':
+            Matrix.result_mat = []
+            self.get_matrix('')
+            for i in range(self.row):
+                Matrix.result_mat.append(self.mat[i][::-1])
+            
+            self.format_matrix()
+        elif toptions == '4':
+            self.get_matrix('') 
+            Matrix.result_mat = self.mat[::-1]
+
+            self.format_matrix()
+
+    def format_matrix(self):
         print('The result is: ')
         for r in Matrix.result_mat:
             print(' '.join(map(str, r)))
         print('')
+
 
 matrix_a = Matrix()
 matrix_a.get_options()
